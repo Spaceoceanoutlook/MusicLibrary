aba7f83634b01053ef4d4b3017ac8b634fc4a5d6
spaceoceanoutlook <spaceocean@outlook.com>
2025-06-28 14:30:36 +0500

    added a route for any song

--- musiclibrary/app.py
+++ musiclibrary/app.py
@@ -1,18 +1,42 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from musiclibrary.json_utils import load_music_data
-from musiclibrary.config import API_TITLE, API_DESCRIPTION
+from musiclibrary.config import API_TITLE, API_DESCRIPTION, MUSIC_DIR, BASE_DIR, UVICORN_HOST, UVICORN_PORT, UVICORN_RELOAD
+import os
 
 app = FastAPI(
     title=API_TITLE,
     description=API_DESCRIPTION,
 )
 
+# Json с данными
 music_data = load_music_data()
+
+# Создание директории для музыки
+os.makedirs(MUSIC_DIR, exist_ok=True)
+
+# Инициализация шаблонов
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+# Раздача статических файлов
+app.mount("/music", StaticFiles(directory=str(MUSIC_DIR)), name="music")
 
 @app.get("/")
 def get_music():
     return music_data
 
+@app.get("/{song}")
+def get_song(request: Request, song: str):
+    return templates.TemplateResponse(
+        "song.html",
+        {
+            "request": request,
+            "song": song,
+            "audio_url": f"/music/{song}"
+        }
+    )
+
 if __name__ == "__main__":
-    uvicorn.run("musiclibrary.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("musiclibrary.app:app", host=UVICORN_HOST, port=UVICORN_PORT, reload=UVICORN_RELOAD)
--- musiclibrary/config.py
+++ musiclibrary/config.py
@@ -1,9 +1,18 @@
 from pathlib import Path
 
-MUSIC_LIBRARY_PATH = Path("/home/valerii/Music/My_music")
+MUSIC_LIBRARY_PATH = Path("/home/valerii/Music")
 MUSIC_DATA_FILENAME = "music_data.json"
 ALLOWED_AUDIO_EXTENSIONS = {".mp3", ".wav", ".flac", ".ogg"}
 
 # Настройки FastAPI
 API_TITLE = "Music Library API"
 API_DESCRIPTION = "API для работы с музыкальной библиотекой"
+
+# Настройки сервера
+UVICORN_HOST = "0.0.0.0"
+UVICORN_PORT = 8000
+UVICORN_RELOAD = True
+
+# Настройки директорий хранения музыки
+MUSIC_DIR = Path("/var/www/music") # Пусть к директории, в которой будет храниться музыка
+BASE_DIR = Path(__file__).parent
